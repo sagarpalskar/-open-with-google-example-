da02dfb9e156987c3d2b486f9cc6d223ee9575f5
sagarp <sagarp@sagarp>
2020-03-16 14:52:41 +0530

    second commit

--- Google_Oauth_API/src/main/java/com/oauth/controller/RestApi.java
+++ Google_Oauth_API/src/main/java/com/oauth/controller/RestApi.java
@@ -1,13 +1,21 @@
 package com.oauth.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oauth.dao.BootDAO;
@@ -17,6 +25,9 @@ import com.oauth.model.Book;
 public class RestApi 
 {
 	static Logger l1 = LogManager.getLogger(RestApi.class.getName());
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@Autowired
 	public BootDAO dao;
@@ -40,5 +51,35 @@ public class RestApi
 		return book;
 	}
 	
+	// can not check in postman  and  not on browser because of post method
+
+/*	@PostMapping("/insertBook")
+	public int insertBook(@RequestBody Book book) 
+	{
+		String sql = "insert into Book values('" + book.getBookId() + "','" +book.getBookName() + "','" 
+	                                          +book.getBookAuthor()+ "','"+book.getBookPrice() + "')";
+		return jdbcTemplate.update(sql);
+	}
 	
+	@PutMapping("/updateBook")
+	public int updateBook(@RequestBody Book book) 
+	{
+		String sql = "insert into Book values('" + book.getBookId() + "','" +book.getBookName() + "','" 
+	                                          +book.getBookAuthor()+ "','"+book.getBookPrice() + "')";
+		return jdbcTemplate.update(sql);
+	}
+	*/
+	
+	@GetMapping("/deleteBookByName/{bookName}")
+	public int deleteBookByName(@PathVariable("bookName") String bookName) 
+	{
+		String sql = "DELETE FROM  Book  WHERE  bookname ='"+bookName+"'";
+		return jdbcTemplate.update(sql);
+	}
+	
+	@RequestMapping("/user")
+	@ResponseBody
+	public Principal user(Principal principal) {
+		return principal;
+	}
 }
